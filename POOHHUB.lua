78e652d23a3a122ab1b6409fa6ef16e93375146c
POOHHUB <128593892+POOHHUB@users.noreply.github.com>
2023-04-10 18:03:15 +0700

    Update POOHHUB.lua

--- POOHHUB.lua
+++ POOHHUB.lua
@@ -1,9 +1,9 @@
 local PL game.PlaceId
 if PL == "9674012909" then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/POOHHUB/script/main/9674012909.lua",true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/POOHHUB/script/main/9674012909.lua"))()
 end
 elseif PL == "12851888521" then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/POOHHUB/script/main/12851888521.lua",true))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/POOHHUB/script/main/12851888521.lua"))()
 end
 else
   game.Players.LocalPlayer:Kick("Game Not support")
